080b4387ed9fab39bc3d8b870ee0570917733926
jjos2372 <jjos2372@gmail.com>
2019-04-20 13:19:37 -0300

    Added sample code

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import bt.Contract;
+import bt.EmulatorWindow;
+
+/**
+ * Modify this class for your new BlockTalk smart contract. 
+ *
+ * Just run this class to start a BlockTalk emulator for testing,
+ * debugging, or compiling to AT bytecode.
+ * 
+ * @author jjos
+ */
+public class App extends Contract {
+
+    @Override
+    public void txReceived() {
+        // In this sample we simply send back funds received (minus activation fee)
+        sendAmount(getCurrentTx().getAmount(), getCurrentTx().getSenderAddress());
+    }
+
+    /**
+     * The main function is for debugging purposes only it will not be
+     * compiled to AT bytecode
+     */
+    public static void main(String[] args) {
+        new EmulatorWindow(App.class);
+    }
+}
